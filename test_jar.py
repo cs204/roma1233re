c3fef792cb9b7b4228fd647f005ea791549e1ea4
test <test>
2024-11-26 09:32:48 +0700

    test

---
+++ test_jar.py
@@ -0,0 +1,21 @@
+from jar import Jar
+
+def test_initial_capacity():
+    jar = Jar(10)
+    assert jar.capacity == 10
+
+def test_default_capacity():
+    jar = Jar()
+    assert jar.capacity == 12
+
+def test_deposit_within_capacity():
+    jar = Jar(5)
+    jar.deposit(3)
+    assert jar.size == 3
+
+def test_withdraw_within_capacity():
+    jar = Jar(5)
+    jar.deposit(3)
+    jar.withdraw(2)
+    assert jar.size == 2
+
